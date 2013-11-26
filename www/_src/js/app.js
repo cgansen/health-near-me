b87feb11d8f18a661be2cd3a4a3593fbccdc4bb8
cgansen <cgansen@gmail.com>
2013-11-26 01:27:09 -0600

    sweet jesus, working permalinks and routing

--- www/_src/js/app.js
+++ www/_src/js/app.js
@@ -1,17 +1,39 @@
 ---
 ---
-var healthApp = angular.module("healthApp", []);
 
-healthApp.controller('SearchCtrl', ['$scope', '$http', function($scope, $http){
-    $scope.searchLocation = "";
+var healthApp = angular.module("healthApp", ['ngRoute'])
+.config(function($routeProvider, $locationProvider){
+    $routeProvider.when("/help", {
+        templateUrl: "help.html",
+        controller: "IndexCtrl",        
+    });
+    
+    $routeProvider.when("/about", {
+        templateUrl: "about.html",
+        controller: "IndexCtrl",        
+    });
+    
+    $routeProvider.when("/", {
+        templateUrl: "search.html",
+        controller: "SearchCtrl",        
+    });    
+});
+
+
+healthApp.controller('IndexCtrl', ['$scope', '$http', function($scope, $http){
+    
+}]);
+
+healthApp.controller('SearchCtrl', ['$scope', '$http', '$location', function($scope, $http, $location){
+    $scope.searchLocation = ($location.search()).l;
     $scope.geocodeError = false;
     $scope.noResultsError = false;
-    $scope.lat = "";
-    $scope.lon = "";
-    $scope.dist = 1609;
-    $scope.searchType = "all";
+    $scope.lat = ($location.search()).lat;
+    $scope.lon = ($location.search()).lon;
+    $scope.dist = ($location.search()).d || 1609;
+    $scope.searchType = ($location.search()).typ;
     
-    $scope.map = new google.maps.Map(document.getElementById("map-canvas"), {zoom: 12, mapTypeId: google.maps.MapTypeId.ROADMAP});
+    // $scope.map = new google.maps.Map(document.getElementById("map-canvas"), {zoom: 12, mapTypeId: google.maps.MapTypeId.ROADMAP});
     
     $scope.doGeocode = function(){
         // hit google for lat/lng
@@ -21,24 +43,40 @@ healthApp.controller('SearchCtrl', ['$scope', '$http', function($scope, $http){
             if (status == google.maps.GeocoderStatus.OK) {
                 $scope.lat = results[0].geometry.location.ob;
                 $scope.lon = results[0].geometry.location.pb;
-                
+                           
                 $scope.doSearch();
                 
-                $scope.map.setCenter(results[0].geometry.location);
-                $scope.map.setZoom(14)
-                var marker = new google.maps.Marker({
-                    map: $scope.map,
-                    position: results[0].geometry.location
-                });
+                // $scope.map.setCenter(results[0].geometry.location);
+                //                     $scope.map.setZoom(14)
+                //                     var marker = new google.maps.Marker({
+                //                         map: $scope.map,
+                //                         position: results[0].geometry.location
+                //                     });
                 $scope.geocodeError = false
             } else {
                 $scope.geocodeError = true
                 console.log("Geocode was not successful for the following reason: " + status);
             }
+            
+            $scope.updatePath();
         });        
     };
     
-    $scope.doSearch = function(){
+    if($location.search().s == "1"){ $scope.doGeocode() };
+    
+    $scope.updatePath = function(){
+        $location.path("/").search({
+            l: $scope.searchLocation, 
+            typ: $scope.searchType, 
+            lat: $scope.lat, 
+            lon: $scope.lon,
+            d: $scope.dist,
+            s: 1
+        });
+    }
+    
+    $scope.doSearch = function(){        
+        
         // hit API for nearby health providers
         $http.jsonp("{{ site.api_url }}/search",
                     { params: { 
